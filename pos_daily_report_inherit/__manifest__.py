5f25db7cea74ded0e774ce90d0265acb3a4b0cd6
Konejova <cvalero.amizola@gmail.com>
2024-05-22 13:41:00 -0400

    pos_daily_report
    Para el reporte diario del restaurante Cash in - Cash out

---
+++ pos_daily_report_inherit/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Cash in - Cash out POS',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': 'Point of Sale',
+    'depends': [
+        'point_of_sale',
+        'pos_daily_sales_reports'
+    ],
+    'data': [
+        'views/point_of_sale_view.xml'
+    ],
+    'demo': [
+        ''
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
